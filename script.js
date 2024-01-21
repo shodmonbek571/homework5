1c83fd31b3a3d3ca37ad2a7e704c6e739fc3e3b7
shodmonbek571 <shodmonbekakbarov820@gmail.com>
2024-01-21 15:09:05 +0500

    Homework

---
+++ script.js
@@ -0,0 +1,69 @@
+//  0 - MASALA
+// for (let i = 1; i <= 100; i++) {
+//     console.log(i)  
+// }
+//   1 - MASALA
+// let n = 15
+// for (let i = 1; i <= n ; i++) {
+//    console.log(i);
+// }
+//   2 - MASALA
+// let n = 20;
+// let res = 0;
+// for (let i = 1; i <= n; i++) {
+//     res+=i;
+//     console.log(res);
+// }
+//  3 - MASALA
+// let n = 25;
+// let result = 0;
+// for (let i = 1; i <= n; i++) {
+//     if (i % 2 == 1) {
+//         result+=i;
+//     } 
+// } 
+// console.log(result);
+//  4 - MASALA
+// let a = 1;
+// let b = 10;
+// let res = 1;
+// for (let i = a; i <= b; i++) {
+//  if (i % 2 == 0) {
+//     res*= i;
+//  }
+// }
+// console.log(res);
+
+//  5 - MASALA
+// let a = 15;
+// let b = 30;
+// let res = 0;
+// for (let i = a; i <= b; i++) {
+//    if ( i % 3 == 0 && i % 5 == 0) {
+//     res++;
+//    }
+    
+// }
+// console.log(res);
+
+//   6 - MASALA
+//let num = 12;
+// let counter = 0;
+// for (let i = 0; i < num ; i++) {
+//     if (i %  2 === 0) {
+//         counter++;
+//     }
+    
+// }
+// console.log(counter);
+
+//  7 - MASALA
+// let num = 27;
+// let counter = 0;
+// for (let i = 1; i <= num; i++) {
+//    if (num % i === 0) {
+//     counter++;
+//    }
+    
+// }
+// console.log(counter);
